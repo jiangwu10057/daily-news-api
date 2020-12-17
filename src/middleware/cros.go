9f5ad5712996dbc4edcddba97fd42a394c59c2a5
jiangwu10057 <jiangwu10057@qq.com>
2020-12-18 05:23:18 +0800

    feat:更新跨域

--- src/middleware/cros.go
+++ src/middleware/cros.go
@@ -10,7 +10,7 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie", "Authorization"}
-	config.AllowOrigins = []string{"http://localhost:8080", "http://127.0.0.1:8080", "http://127.0.0.1", "http://localhost", "http://127.0.0.1:22554"}
+	config.AllowOrigins = []string{"http://localhost:8080", "http://127.0.0.1:8080", "http://news.taoerqu.com", "https://news.taoerqu.com"}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
